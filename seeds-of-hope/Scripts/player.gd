a835a34f9a6dc7c1feeb1f03c67e04528bf48434
Teuxml <85766646+Teuxml@users.noreply.github.com>
2024-12-18 20:38:10 -0300

    initial fix-up
    added accelerated movement, a background and fixed the resolution

--- seeds-of-hope/Scripts/player.gd
+++ seeds-of-hope/Scripts/player.gd
@@ -1,33 +1,35 @@
 extends CharacterBody2D
 
-const speed = 200
-const acceleration = 100
-const deceleration = 200
+@export var max_speed : float = 80
+@export var acceleration_time : float ## The time it takes you to reach maximum speed, in seconds
+
+@onready var acceleration = max_speed/acceleration_time
+@onready var sprite = $AnimatedSprite2D
+
+var target_direction : Vector2
+var direction : Vector2 = Vector2.ZERO
+var speed : float
+var time : float
+var move_time : float
 
 func _physics_process(delta):
-	player_movement(delta)
-
-func player_movement(delta):
-	var direction = Vector2.ZERO 
-	
-	
-	if Input.is_action_pressed("ui_right"):
-		direction.x += 1
-	if Input.is_action_pressed("ui_left"):
-		direction.x -= 1
-	
-	
-	if Input.is_action_pressed("ui_down"):
-		direction.y += 1
-	if Input.is_action_pressed("ui_up"):
-		direction.y -= 1
-
-	
-	direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down").normalized()
-
-	
-	if velocity.length() < speed:
-		velocity+=direction*acceleration
-
-	
+	time += delta
+	target_direction = Input.get_vector("left","right","up","down").normalized()
+	if target_direction:
+		if speed < max_speed:
+			speed += acceleration * delta
+		else: speed = max_speed
+	if !target_direction:
+		speed = 0
+	direction = target_direction
+	direction.x = round(direction.x)
+	direction.y = round(direction.y)
+	direction = direction.normalized()
+	print(speed)
+	velocity = direction * speed
 	move_and_slide()
+
+func _process(delta: float) -> void:
+	#sprite.global_position.x = round(self.global_position.x)
+	#sprite.global_position.y = round(self.global_position.y)
+	pass
