ebeee4e06b68f4e3b50905f28f057e0e067af0c3
36sweaters <the36sweaters@gmail.com>
2025-01-03 11:29:05 +0900

    removed redundancies in clock

--- seeds-of-hope/Scripts/clock.gd
+++ seeds-of-hope/Scripts/clock.gd
@@ -4,20 +4,20 @@ extends CanvasLayer
 @onready var timer = $Timer
 @onready var label = $Label
 
-#not sure if we'll need these but might be good to have anyways
+#not sure if we'll need these but might be good to have anyways (minutes are in groups of 10 currently)
 const hours_in_day = 24
-const minutes_in_hour = 60
-const minutes_in_day = 1440
+const minutes_in_hour = 6
+const minutes_in_day = 144
 
 #the actual result of what the clock does
 @export var current_hour = 8
 @export var current_minutes = 0
-var extra_zero = "0"
+
 
 #starts the timer when the clock is brought into the scene
 func _ready():
 	timer.start()
-	label.text = (str(current_hour) + ":" + str(current_minutes) + extra_zero)
+	label.text = (str(current_hour) + ":" + str(current_minutes) + "0")
 	
 #calls the tick function whenever the timer runs out. This timer is assumed to be equal to 10 minutes of in-game time.
 func _on_timer_timeout():
@@ -25,18 +25,16 @@ func _on_timer_timeout():
 
 #updates everything that needs to be updated every 10 minutes of in game time. For now it should just be the clock.
 func tick():
-	extra_zero = ""
-	current_minutes += 10
+	current_minutes += 1
 	#updates the hour if minutes = 60
 	if current_minutes == minutes_in_hour:
 		current_hour += 1
 		current_minutes = 0
-		extra_zero = "0"
 	#runs the day_tick function if a day has been completed
 	if current_hour == hours_in_day:
 		current_hour = 0
 		day_tick()
-	label.text = (str(current_hour) + ":" + str(current_minutes) + extra_zero)
+	label.text = (str(current_hour) + ":" + str(current_minutes) + "0")
 #updates everything that needs to be updated every day
 func day_tick():
 	pass
